bcc3cfbb66c3cb37139be327e51632e1d382a7ca
JellyWX <judewrs@gmail.com>
2017-08-15 00:01:24 +0100

    added sorting methods and some other variables

--- src/game.py
+++ src/game.py
@@ -8,14 +8,25 @@ class Game(Widget):
   started = False
   waves = []
   towers = []
+  path = []
 
   def __init__(self,*args,**kwargs):
     super(Game,self).__init__(*args,**kwargs)
     Builder.load_file('game.kv') ## load the game's assigned layout file ##
     Clock.schedule_interval(self.update,1/20)
 
-  def set_level(self,level):
+  def set_level(self,level): ## method for loading levels from files
     pass
 
   def update(self,t):
     if started:
+      self.sort_widgets()
+
+  def sort_widgets(self): ## method to make sure objects render in the correct order
+    children_sorted = sorted(self.children,key=lambda w: w.y)
+    children_sorted.reverse()
+
+    if children_sorted != self.children:
+      for i in children_sorted:
+        self.remove_widget(i)
+        self.add_widget(i)
