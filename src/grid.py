f326c7f511b0c92cbe044acf68fb39f13b95eb64
JellyWX <judewrs@gmail.com>
2017-08-25 22:28:05 +0100

    added more assets and prettied up some of the existing assets

--- src/grid.py
+++ src/grid.py
@@ -1,8 +1,25 @@
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+
+from random import choice
 
 from locations import ASSETS
 
 
 class Grid(Widget):
 
-  src = ASSETS + 'TowerPointers/Grid.png'
+  buildable = True
+  travellable = True
+
+  rotation = NumericProperty(choice((0,90,180)))
+
+  src = ASSETS + 'Grid/RustyPanel1.png'
+  if buildable:
+    overlay = ASSETS + 'Grid/overlay.png'
+  else:
+    overlay = ASSETS + 'placeholder/invisible.png'
+
+  def __init__(self,*args,**kwargs):
+    super(Grid,self).__init__(*args,**kwargs)
+    self.rotation = choice((0,90,180))
+    print(self.rotation)
