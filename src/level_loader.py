ee7bc34ca7f28d3404a9d27bfa9a88f7bdef4d87
jude-Z87-HD3 <judewrs@gmail.com>
2017-08-27 02:58:06 +0100

    untested map-loader

--- src/level_loader.py
+++ src/level_loader.py
@@ -1,3 +1,4 @@
+from grid import Grid
 from locations import LEVELS
 
 
@@ -6,7 +7,8 @@ class Level(object):
   def __init__(self,level):
     self.level = level
     self.name = level
-    self.size = (24,15)
+    self.size = (16,10)
+    self.map = []
 
     with open(LEVELS + level + '/data','r') as f:
       for l in f:
@@ -18,12 +20,41 @@ class Level(object):
 
           self.name = name
 
-        elif l.startswith('size='):
-          size = l.split('=')
-          size.pop(0)
-          size = ''.join(size)
-          size = size.split(',')
+        #elif l.startswith('size='):
+        #  size = l.split('=')
+        #  size.pop(0)
+        #  size = ''.join(size)
+#          size = size.split(',')
+#
+#          size = map(float,size)
+#
+#          self.size = tuple(size)
 
-          size = map(float,size)
+    #self.map = [[Grid() for i in range(int(self.grid_size[1]))] for i in range(int(self.grid_size[0]))]
 
-          self.size = tuple(size)
+    with open(LEVELS + level + '/map','r') as f:
+      row = 0
+      for l in f:
+        self.map.append([])
+
+        if l.startswith('#'):
+          continue
+
+        row = l.split(',')
+        for item in row:
+          if item in ['0', 'all', 'bt']:
+            self.map[row].append(Grid())
+
+          elif item in ['1', 't']:
+            self.map[row].append(Grid(buildable=False))
+
+          elif item in ['2', 'b']:
+            self.map[row].append(Grid(travellable=False))
+
+          elif item in ['3', 'n', 'none']:
+            self.map[row].append(Grid(buildable=False,travellable=False))
+
+          else:
+            raise ValueError('Did not expect character \'' + str(item) + '\' in map declaration.')
+            
+        row += 1
